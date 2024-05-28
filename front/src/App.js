bc73026fac21b5338f9b476aecdf70de26d3b723
zzannorita <tpals0518@naver.com>
2024-05-28 21:47:33 +0900

    Fix: 예시 문구 삭제

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/navi/Login";
-import MainPage from "./components/navi/MainPage";
+// import MainPage from "./components/navi/MainPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>초리 유디 화잇팅</div>
         <div>
           <Login></Login>
-          <div>컴포넌트들 테스트</div>
-          <MainPage></MainPage>
+          {/* <div>컴포넌트들 테스트</div>
+          <MainPage></MainPage> */}
         </div>
       </header>
     </div>
