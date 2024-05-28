4ef074fdcce86cbe76e2c54896085f533ce9983b
zzannorita <tpals0518@naver.com>
2024-05-28 21:50:02 +0900

    Design: 홈페이지 css 수정

--- front/src/components/navi/Login.js
+++ front/src/components/navi/Login.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import logo from "../../img/logo.png";
+import calendar from "../../img/calendar.png";
+import styles from "./Login.module.css";
 
 const Login = () => {
   const [usercode, setUserCode] = useState("");
@@ -35,25 +38,58 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div>처음 화면이 될 것</div>
-      <div> db 테스트용 input</div>
-      <div>
-        <input
-          type="text"
-          value={usercode}
-          onChange={handleUsercodeChange}
-          placeholder="유저 코드를 입력하세요"
-        />
-        <input
-          type="text"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder="패스워드를 입력하세요"
-        />
+    <div className={styles.container}>
+      <div className={styles.headerBox}>
+        <div className={styles.logoBox}>
+          <img src={logo} alt="logo" width="200px" height="74px"></img>
+        </div>
       </div>
-      <button onClick={handleSubmit}>click</button>
-      <div>{usercode}</div>
+      <div className={styles.mainBox}>
+        <div className={styles.mainDiv}>
+          <div className={styles.leftBox}>
+            <div className={styles.textBox}>
+              <div>
+                오늘 <span className={styles.highlight}>할</span> 일을
+                정리해보세요!
+              </div>
+              <div>
+                일상 속 작은 변화로 큰 편의를. 심플하고 효율적인 일정 관리
+                서비스
+              </div>
+            </div>
+            <div className={styles.inputBox}>
+              <input
+                className={styles.userCode}
+                type="text"
+                value={usercode}
+                onChange={handleUsercodeChange}
+                placeholder="코드를 입력하세요."
+              />
+              {/* <input
+                className={styles.pwd}
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                placeholder="패스워드를 입력하세요"
+              />
+              <button onClick={handleSubmit}>click</button>
+              <div>{usercode}</div> */}
+              <div className={styles.tipBox}>
+                <span className={styles.highlight}>TIP</span> 처음이신가요?
+                새로운 코드(10자리 이내)를 입력해 주세요.{" "}
+              </div>
+            </div>
+          </div>
+          <div className={styles.rightBox}>
+            <img
+              src={calendar}
+              alt="calendar"
+              width="350px"
+              height="350px"
+            ></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
