451877d3f51f05cecf2c082ebfc3e6f5adfbea6a
zzannorita <tpals0518@naver.com>
2024-05-31 15:48:31 +0900

    Design: 메인페이지 헤더 생성

--- front/src/components/navi/MainPage.js
+++ front/src/components/navi/MainPage.js
@@ -1,15 +1,46 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import Board from "../component/Board";
 import Calender from "../component/Calender";
 import Gantt from "../component/Gantt";
+import styles from "./Login.module.css";
+import logo from "../../img/logo.png";
+import view from "../../img/view.png";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
+      <div className={styles.header}>
+        <div className={styles.logoBox}>
+          <img src={logo} alt="logo" width="200px" height="74px"></img>
+        </div>
+        <div className={styles.headerBox}>
+          <div className={styles.naviBox}>
+            <div className={styles.naviInnerBox}>
+              <div onClick={() => navigate("/calendar")}>캘린더</div>
+              <div onClick={() => navigate("/gantt")}>간트차트</div>
+              <div onClick={() => navigate("/todo")}>할 일</div>
+            </div>
+            <div className={styles.viewImg}>
+              <img src={view} alt="view"></img>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>
-        <Board></Board>
-        <Calender></Calender>
-        <Gantt></Gantt>
+        <Routes>
+          <Route path="/calendar" element={<Calender />} />
+          <Route path="/gantt" element={<Gantt />} />
+          <Route path="/todo" element={<Board />} />
+          <Route path="/" element={<Calender />} />
+        </Routes>
       </div>
     </div>
   );
