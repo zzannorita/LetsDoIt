3e00e8b3fcd4f8f54652015a0c4fdc89fcd079b0
kyuchory <sls789456@naver.com>
2024-05-31 16:18:29 +0900

    Refactor : 데이터베이스 비밀번호 변경

--- back/db.js
+++ back/db.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "manager",
-  password: "test1234",
+  password: "1234",
   database: "todo",
 });
 
