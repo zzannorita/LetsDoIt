c840e9bff92a50fa62c570311b1fc97f1588e283
zzannorita <tpals0518@naver.com>
2024-06-10 19:05:35 +0900

    Test: 메인페이지 캘린더로 임시 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/navi/Login";
-// import MainPage from "./components/navi/MainPage";
+import MainPage from "./components/navi/MainPage";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <Login></Login>
-          {/* <div>컴포넌트들 테스트</div>
-          <MainPage></MainPage> */}
+          {/* <Login></Login>
+          <div>컴포넌트들 테스트</div> */}
+          {/* <MainPage></MainPage> */}
+          <Router>
+            <MainPage />
+          </Router>
         </div>
       </header>
     </div>
